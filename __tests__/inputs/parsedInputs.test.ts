4fb128950acf174a6b8ceddfd5bcd51e7611bdf6
Benjamin Lei <9943953+benlei@users.noreply.github.com>
2024-10-08 06:49:08 +0000

    adding tests for parsed inputs

--- __tests__/inputs/parsedInputs.test.ts
+++ __tests__/inputs/parsedInputs.test.ts
@@ -1,6 +1,12 @@
 import { describe, beforeEach, it, expect, vi } from 'vitest'
 import * as inputs from '../../src/inputs/coreInputs'
-import { repository } from '../../src/inputs'
+import {
+  issueLabels,
+  issueNumber,
+  issueState,
+  repository
+} from '../../src/inputs'
+import { filterOptions } from '../../src/inputs/parsedInputs'
 
 describe('repository', () => {
   beforeEach(() => {
@@ -27,5 +33,68 @@ describe('repository', () => {
 
     vi.spyOn(inputs, 'repositoryInput').mockReturnValue('/bar')
     expect(() => repository()).toThrow()
+  })
+})
+
+describe('issueNumber', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should parse issue number properly', () => {
+    vi.spyOn(inputs, 'issueNumberInput').mockReturnValue('1236')
+    expect(issueNumber()).toBe(1236)
+  })
+})
+
+describe('issueState', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should parse issue state properly', () => {
+    vi.spyOn(inputs, 'stateInput').mockReturnValue('open')
+    expect(issueState()).toBe('open')
+
+    vi.spyOn(inputs, 'stateInput').mockReturnValue('closed')
+    expect(issueState()).toBe('closed')
+
+    vi.spyOn(inputs, 'stateInput').mockReturnValue('all')
+    expect(issueState()).toBe('all')
+
+    vi.spyOn(inputs, 'stateInput').mockReturnValue('invalid')
+    expect(issueState()).toBe('open')
+  })
+
+  it('should default to open if state is not valid', () => {
+    vi.spyOn(inputs, 'stateInput').mockReturnValue('')
+    expect(issueState()).toBe('open')
+  })
+})
+
+describe('issueLabels', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should parse issue labels properly', () => {
+    vi.spyOn(inputs, 'labelsInput').mockReturnValue('foo,bar')
+    expect(issueLabels()).toBe('foo,bar')
+
+    vi.spyOn(inputs, 'labelsInput').mockReturnValue('')
+    expect(issueLabels()).toBeUndefined()
+  })
+})
+
+describe('filterOptions', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should return filter options properly', () => {
+    vi.spyOn(inputs, 'stateInput').mockReturnValue('open')
+    vi.spyOn(inputs, 'labelsInput').mockReturnValue('foo,bar')
+
+    expect(filterOptions()).toEqual({ state: 'open', labels: 'foo,bar' })
   })
 })
